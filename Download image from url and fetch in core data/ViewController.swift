e30be3f50253038412b268148f26385b7caed89d
Mohit Andhare <mohitandhare@skromanglobal.com>
2022-07-14 14:52:14 +0530

    Initial Commit

---
+++ Download image from url and fetch in core data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Download image from url and fetch in core data
+//
+//  Created by Developer Skromanglobal on 14/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
